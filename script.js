4d89ffa2e13ae671b1d0525c752891d7ee4d5b9c
Fatemeh madihi <54662177+fmadihi@users.noreply.github.com>
2023-11-07 21:28:18 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,61 @@
-(function(){
-/*check input not empty*/
-document.getElementById("btn1").addEventListener("click",function(){
-    var item = document.getElementById("item").value;
-    (item=="") ? alert("input is invalid") : listWrite();
-})
-/*input is valid */
-function listWrite(){
-    /*craete element with img to add before #btn2*/
-    const span = document.createElement("span");
-    const div = document.createElement("div");
-    const img = document.createElement("img");
+(function () {
 
-    /*set src to img */
-    img.setAttribute("src","./image/delete.png");
-    /*set input value to span*/
-    span.innerHTML = item.value;
-    div.appendChild(span);
-    div.appendChild(img)
-    
-    const elem = document.getElementById("frm");
-    elem.insertBefore(div,document.getElementById('btn2'));
-}
+    /*check input not empty*/
+    document.getElementById("btn1").addEventListener("click", function () {
+        var item = document.getElementById("item").value;
+        (item == "") ? alert("input is invalid") : listWrite();
+    })
+    /*input is valid */
+    function listWrite() {
+        /*craete element with img to add before #btn2*/
+        const span = document.createElement("span");
+        const div = document.createElement("div");
+        const img = document.createElement("img");
 
-/*second box----> delete all member in the list*/
-document.getElementById("btn2").addEventListener("click",function(){
-    alert("ok")
-})
+        /*set src ana event to img */
+        img.setAttribute("src", "./image/delete.png");
+        img.setAttribute("onclick", "singleDelete(this)");
+        /*set input value to span*/
+        span.innerHTML = item.value;
+        div.appendChild(span);
+        div.appendChild(img)
 
+        const elem = document.getElementById("frm");
+        elem.insertBefore(div, document.getElementById('btn2'));
+        item.value = "";
+
+        /*show and hide sectio "done successfully"*/
+        shown();
+        hiden();
+        /*end show and hide sectio "done successfully"*/
+    }
+
+    /*second box----> delete all member in the list*/
+    document.getElementById("btn2").addEventListener("click", function () {
+        const elem = document.querySelectorAll("div");
+        elem.forEach((e) => {
+            e.remove();
+            shown();
+            hiden();
+        });
+
+    })
 })();
+/*delete single item*/
+function singleDelete(e) {
+    e.parentNode.remove();
+    shown();
+    hiden();
+}
+function shown() {
+    setTimeout(() => {
+        const box = document.getElementById("show");
+        box.style.display = 'block';
+    }, 10);
+}
+function hiden() {
+    setTimeout(() => {
+        const box = document.getElementById('show');
+        box.style.display = 'none';
+    }, 3000);
+}
